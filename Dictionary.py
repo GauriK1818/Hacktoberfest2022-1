24ba9cb923489f354060cfc96346c4b4ee10e5b0
GauriK1818 <87660744+GauriK1818@users.noreply.github.com>
2022-10-23 21:19:47 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Gauri', 2: 'Hello', 3: 'World'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Gauri', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
